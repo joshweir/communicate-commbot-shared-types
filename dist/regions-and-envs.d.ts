428812847e860ef9a06238f6e3b55e8ad41c3b10
joshweir <joshua.weir@gmail.com>
2019-08-16 15:58:47 +1000

    build and bump version

--- dist/regions-and-envs.d.ts
+++ dist/regions-and-envs.d.ts
@@ -1,10 +1,10 @@
 export declare const regions: ["aus", "nova"];
 export declare type Region = (typeof regions)[number];
-export declare type RegionIncAll = Region | 'all';
+export declare type RegionIncAll = Region | "all";
 export declare const appEnvironments: ["dev", "staging", "qa", "beta", "live"];
 export declare type AppEnv = (typeof appEnvironments)[number];
 export declare const dataEnvironments: ["com-datastaging", "com-datalive"];
 export declare type DataEnv = (typeof dataEnvironments)[number];
-export declare type DataEnvIncAll = DataEnv | 'all';
+export declare type DataEnvIncAll = DataEnv | "all";
 export declare const mapDataEnvsFromAllEnvs: (envs: string[]) => ("com-datastaging" | "com-datalive")[];
 //# sourceMappingURL=regions-and-envs.d.ts.map
--- dist/regions-and-envs.js
+++ dist/regions-and-envs.js
@@ -1,24 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.regions = ['aus', 'nova'];
-exports.appEnvironments = ['dev', 'staging', 'qa', 'beta', 'live'];
-exports.dataEnvironments = ['com-datastaging', 'com-datalive'];
-const liveDataEnvProcessingEnvs = ['qa', 'beta', 'live', 'com-datalive'];
+exports.regions = ["aus", "nova"];
+exports.appEnvironments = ["dev", "staging", "qa", "beta", "live"];
+exports.dataEnvironments = ["com-datastaging", "com-datalive"];
+const liveDataEnvProcessingEnvs = ["qa", "beta", "live", "com-datalive"];
 exports.mapDataEnvsFromAllEnvs = (envs) => {
     const envsLowerCase = envs.map(s => s.toLocaleLowerCase());
-    if (envsLowerCase.indexOf('all') > -1) {
-        return ['com-datastaging', 'com-datalive'];
+    if (envsLowerCase.indexOf("all") > -1) {
+        return ["com-datastaging", "com-datalive"];
     }
     const dataEnvs = [];
-    if (envsLowerCase.find((env) => {
-        return liveDataEnvProcessingEnvs.find((p) => p === env);
+    if (envsLowerCase.find(env => {
+        return liveDataEnvProcessingEnvs.find(p => p === env);
     })) {
-        dataEnvs.push('com-datalive');
+        dataEnvs.push("com-datalive");
     }
-    if (envsLowerCase.find((env) => {
-        return liveDataEnvProcessingEnvs.find((p) => p !== env);
+    if (envsLowerCase.find(env => {
+        return !liveDataEnvProcessingEnvs.find(p => p === env);
     })) {
-        dataEnvs.push('com-datastaging');
+        dataEnvs.push("com-datastaging");
     }
     return dataEnvs;
 };
