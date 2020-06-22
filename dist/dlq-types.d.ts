f31936d2f091ef8c25cadf0e81f942a340168d87
joshweir <joshua.weir@gmail.com>
2020-06-22 13:14:56 +1000

    build

--- dist/dlq-types.d.ts
+++ dist/dlq-types.d.ts
@@ -1,5 +1,15 @@
-import { Region } from './regions-and-envs';
+import { SQSMessageAttribute } from "aws-lambda";
+import { Region } from "./regions-and-envs";
 export declare namespace Dlq {
+    type SQSRecordAttributes = {
+        ApproximateReceiveCount: string;
+        SentTimestamp: string;
+        SenderId: string;
+        ApproximateFirstReceiveTimestamp: string;
+        SequenceNumber?: string;
+        MessageGroupId?: string;
+        MessageDeduplicationId?: string;
+    };
     type TDlqMessage = {
         messageId: string;
         env: string;
@@ -8,8 +18,11 @@ export declare namespace Dlq {
         region: Region;
         operationId?: string;
         payload: string;
+        attributes?: SQSRecordAttributes;
+        messageAttributes?: Record<string, SQSMessageAttribute>;
     };
+    const isSQSRecordAttributes: (thing: any) => thing is SQSRecordAttributes;
     const isDlqMessage: (thing: any) => thing is TDlqMessage;
-    type TDlqMessageKey = Pick<TDlqMessage, 'region' | 'dlqName' | 'env' | 'messageId'>;
+    type TDlqMessageKey = Pick<TDlqMessage, "region" | "dlqName" | "env" | "messageId">;
 }
 //# sourceMappingURL=dlq-types.d.ts.map
--- dist/dlq-types.js
+++ dist/dlq-types.js
@@ -3,15 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const regions_and_envs_1 = require("./regions-and-envs");
 var Dlq;
 (function (Dlq) {
+    Dlq.isSQSRecordAttributes = (thing) => typeof thing === 'object' &&
+        typeof thing.ApproximateReceiveCount === 'string' &&
+        typeof thing.SentTimestamp === 'string' &&
+        typeof thing.SenderId === 'string' &&
+        typeof thing.ApproximateFirstReceiveTimestamp === 'string';
     Dlq.isDlqMessage = (thing) => thing &&
-        typeof thing.env === 'string' &&
-        typeof thing.dlqName === 'string' &&
-        typeof thing.messageId === 'string' &&
-        typeof thing.sentTimestamp === 'number' &&
-        typeof thing.region === 'string' &&
+        typeof thing.env === "string" &&
+        typeof thing.dlqName === "string" &&
+        typeof thing.messageId === "string" &&
+        typeof thing.sentTimestamp === "number" &&
+        typeof thing.region === "string" &&
         regions_and_envs_1.regions.indexOf(thing.region) > -1 &&
-        ['string', 'undefined'].indexOf(typeof thing.operationId) > -1 &&
-        thing.payload;
+        ["string", "undefined"].indexOf(typeof thing.operationId) > -1 &&
+        thing.payload &&
+        (typeof thing.attributes === "undefined" || Dlq.isSQSRecordAttributes(thing.attributes));
 })(Dlq = exports.Dlq || (exports.Dlq = {}));
-;
 //# sourceMappingURL=dlq-types.js.map
